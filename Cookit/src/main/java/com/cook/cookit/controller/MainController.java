1e5f278bcbe2d2c801d019afc83a3b8abc03898b
seouna <ckrgody40@naver.com>
2023-08-28 16:16:22 +0900

    login-page

--- Cookit/src/main/java/com/cook/cookit/controller/MainController.java
+++ Cookit/src/main/java/com/cook/cookit/controller/MainController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,11 +22,14 @@ public class MainController {
 	@RequestMapping(value = "main", method = RequestMethod.GET)
 	public String main(Model model,HttpSession session, HttpServletRequest request) {
 		
-		
 		return "main";
 
 	}
 	
+	@GetMapping("/join")
+	public String join() {
+		return "join";
+	}
 
 	
 	
---
+++ Cookit/src/main/webapp/WEB-INF/views/join.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<c:set var="path" value="${pageContext.request.contextPath}" />
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>쿠킷</title>
+<link href="${path }/resources/css/sub.css" rel="stylesheet">
+</head>
+<body>
+<jsp:include page="nav.jsp" />
+<jsp:include page="footer.jsp" />
+</body>
+</html>
